e15afc8dad370c98ad5f5e4d3d848e271b5a6019
Gaurav Tarlok Kakkar <gaurav21776@gmail.com>
2024-09-29 16:19:15 -0400

    new members

--- submit_expenses.py
+++ submit_expenses.py
@@ -14,20 +14,16 @@ sObj = Splitwise(cus_key ,cus_secret , api_key=api_key)
 print(f"Hello {sObj.getCurrentUser().first_name} {sObj.getCurrentUser().last_name}")
 
 expenses_to_add = [
-                    # ("Doordash Dec 4", 13.32, ("ritesh", "gaurav")),
-                    # ("Blue India", 35, ("ritesh", "gaurav")),
-                    # ("Lyft Nov 11", 8.99, ("ALL")),
-                    # ("Wash Hotel", 251.36, ("nidhima", "gaurav")),
-                    # ("Lyft Home -> Rental Nov 23", 28.74, ("ALL")),
-                    # ("Lyft Rental -> Home Nov 25", 20.99, ("ALL")),
-                    # ("Lyft Rental -> Home Dec 2", 8.92, ("ALL")),
-                    # ("Dec Rent", 1825, ("ritesh", "gaurav")),
-                    # ("Dec Utils", 219.36, ("ALL")),
-                    # ("Nov Elec", 142.95, ("ALL")),
-                    ("Airbnb", 174.56, ("ritesh", "gaurav")),
+                    
+                    
+                    ("Publix", 37.14, ("ALL")),
                     ]
-
-Group_ID = next(grp.getId() for grp in sObj.getGroups() if grp.getName() == "241")
+Group_ID = None
+for grp in sObj.getGroups():
+    if grp.getName() == "Mainu-Tainu":
+        Group_ID = grp.getId()
+# Group_ID = next(grp.getId() for grp in sObj.getGroups() if grp.getName() == "Mainu-Tainu")
+assert Group_ID is not None, "Group ID not found"
 users_map = {}
 for user in sObj.getGroup(Group_ID).getMembers():
     users_map[user.getFirstName().lower()] = user
@@ -35,7 +31,7 @@ for user in sObj.getGroup(Group_ID).getMembers():
 for exp in expenses_to_add:
     splitwise_exp  = Expense()
     splitwise_exp.setCurrencyCode("USD")
-    splitwise_exp.setDescription(f"{exp[0]} Via API")
+    splitwise_exp.setDescription(f"{exp[0]}")
     splitwise_exp.setCost(exp[1])
     splitwise_exp.setGroupId(Group_ID)
     if "ALL" in exp[2]:
